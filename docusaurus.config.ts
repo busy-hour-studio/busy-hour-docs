5369f22f98acccb897683a2b7714d0e04c33cba6
Muhammad Firdaus Sati <firdaus.sati51@gmail.com>
2023-11-05 18:41:19 +0700

    chore: upgrade packages version

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,25 +1,27 @@
-// @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+import { themes } from 'prism-react-renderer';
+import type { Config } from '@docusaurus/types';
 
-/** @type {import('@docusaurus/types').Config} */
+const lightCodeTheme = themes.github;
+const darkCodeTheme = themes.dracula;
+
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Busy Hour',
+  tagline:
+    'Built-in in-app messaging, video & audio calls, and activity feeds wihout headaches.',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://docs.busyhour.id',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'busyhour', // Usually your GitHub org/user name.
+  projectName: 'busy-hour-doc', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
@@ -36,7 +38,7 @@ const config = {
     [
       'classic',
       /** @type {import('@docusaurus/preset-classic').Options} */
-      ({
+      {
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
@@ -54,19 +56,19 @@ const config = {
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
-      }),
+      },
     ],
   ],
 
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-    ({
+    {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'My Site',
+        title: 'Busy Hour',
         logo: {
-          alt: 'My Site Logo',
+          alt: 'Busy Hour Logo',
           src: 'img/logo.svg',
         },
         items: [
@@ -76,7 +78,7 @@ const config = {
             position: 'left',
             label: 'Tutorial',
           },
-          {to: '/blog', label: 'Blog', position: 'left'},
+          { to: '/blog', label: 'Blog', position: 'left' },
           {
             href: 'https://github.com/facebook/docusaurus',
             label: 'GitHub',
@@ -91,7 +93,23 @@ const config = {
             title: 'Docs',
             items: [
               {
-                label: 'Tutorial',
+                label: 'Introduction',
+                to: '/docs/intro',
+              },
+              {
+                label: 'React',
+                to: '/docs/react',
+              },
+              {
+                label: 'React Native',
+                to: '/docs/react-native',
+              },
+              {
+                label: 'Node.js',
+                to: '/docs/nodejs',
+              },
+              {
+                label: 'More',
                 to: '/docs/intro',
               },
             ],
@@ -100,16 +118,8 @@ const config = {
             title: 'Community',
             items: [
               {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-              },
-              {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
-              },
-              {
                 label: 'Twitter',
-                href: 'https://twitter.com/docusaurus',
+                href: 'https://x.com/busyhourcare',
               },
             ],
           },
@@ -121,19 +131,23 @@ const config = {
                 to: '/blog',
               },
               {
+                label: 'Dashboard',
+                href: 'https://busyhour.id/dashboard',
+              },
+              {
                 label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
+                href: 'https://github.com/busy-hour-studio',
               },
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} Busy Hour. Built with Docusaurus.`,
       },
       prism: {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
       },
-    }),
-};
+    },
+} satisfies Config;
 
 module.exports = config;
