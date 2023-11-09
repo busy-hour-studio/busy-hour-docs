312fe097a0ae78fed3d640edf55ea6ab38226add
Muhammad Firdaus Sati <firdaus.sati51@gmail.com>
2023-11-09 20:43:20 +0700

    fix: base url for github page custom domain

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -16,7 +16,7 @@ const config = {
   url: 'https://docs.busyhour.id',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/busy-hour-docs/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
